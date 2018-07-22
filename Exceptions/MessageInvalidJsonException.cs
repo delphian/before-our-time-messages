0c89ca2d9d53dd2f26004e7a497860be4d12d45b
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2018-07-21 23:38:22 -0700

    Added some exceptions

---
+++ Exceptions/MessageInvalidJsonException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeforeOurTime.Models.Exceptions
+{
+    public class MessageInvalidJsonException : Exception
+    {
+        public MessageInvalidJsonException()
+        {
+        }
+
+        public MessageInvalidJsonException(string message)
+            : base(message)
+        {
+        }
+
+        public MessageInvalidJsonException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
---
+++ Exceptions/MessageResponseTimeoutException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeforeOurTime.Models.Exceptions
+{
+    public class MessageResponseTimeoutException : Exception
+    {
+        public MessageResponseTimeoutException()
+        {
+        }
+
+        public MessageResponseTimeoutException(string message)
+            : base(message)
+        {
+        }
+
+        public MessageResponseTimeoutException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
