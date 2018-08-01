34f0a5081579e6535768ed15018b303e1d611b29
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2018-07-31 22:52:22 -0700

    New message to create a new location quickly

---
+++ Exceptions/AuthenticationDeniedException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeforeOurTime.Models.Exceptions
+{
+    public class AuthenticationDeniedException : Exception
+    {
+        public AuthenticationDeniedException()
+        {
+        }
+
+        public AuthenticationDeniedException(string message)
+            : base(message)
+        {
+        }
+
+        public AuthenticationDeniedException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
---
+++ Messages/Requests/LocationAttributes/CreateLocationQuickRequest.cs
@@ -0,0 +1,26 @@
+﻿using BeforeOurTime.Models.Json;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeforeOurTime.Models.Messages.Requests.LocationAttributes
+{
+    public class CreateLocationQuickRequest : Request, IRequest
+    {
+        /// <summary>
+        /// Our unique message identifier
+        /// </summary>
+        [JsonIgnore]
+        [JsonConverter(typeof(GuidJsonConverter))]
+        public static Guid _Id = new Guid("100a31d2-24b8-4bf0-80e6-94ba88f0f5b9");
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public CreateLocationQuickRequest()
+        {
+            this.MessageId = _Id;
+            this.MessageName = "Create Location Quick Request";
+        }
+    }
+}
