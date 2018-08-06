7c06771e8a55f8186465897d25e01b6ca0d2e8ad
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2018-08-05 17:57:25 -0700

    Removing authorization classes

--- Accounts/Account.cs
+++ Accounts/Account.cs
@@ -1,5 +1,4 @@
-﻿using BeforeOurTime.Models.Accounts.Authorization;
-using BeforeOurTime.Models.Items;
+﻿using BeforeOurTime.Models.Items;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -13,7 +12,6 @@ namespace BeforeOurTime.Models.Accounts
     /// </summary>
     public class Account : Model
     {
-        private List<AuthorizationGroup> _groups;
         /// <summary>
         /// Human friendly identifier for account
         /// </summary>
@@ -24,29 +22,5 @@ namespace BeforeOurTime.Models.Accounts
         /// </summary>
         [JsonProperty(PropertyName = "characters", Order = 200)]
         public List<Item> Characters { set; get; } = new List<Item>();
-        /// <summary>
-        /// List of security group membership records. Assigns group membership to an account
-        /// </summary>
-        [JsonProperty(PropertyName = "groupLinks", Order = 300)]
-        public List<AuthorizationAccountGroup> AccountGroups { set; get; }
-        /// <summary>
-        /// List of security groups assigned to account
-        /// </summary>
-        /// <remarks>
-        /// Read only property for delivery to client
-        /// </remarks>
-        [JsonIgnore]
-        public List<AuthorizationGroup> Groups
-        {
-            private set { }
-            get
-            {
-                if (this._groups == null)
-                {
-                    this._groups = this.AccountGroups?.Select(x => x.Group).ToList();
-                }
-                return this._groups;
-            }
-        }
     }
 }
