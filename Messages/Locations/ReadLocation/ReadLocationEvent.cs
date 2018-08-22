5600706a96dd95b9d0723f628c28be0142c7c876
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2018-08-21 23:20:28 -0700

    Adding read location message

---
+++ Messages/Locations/ReadLocation/ReadLocationEvent.cs
@@ -0,0 +1,41 @@
+﻿using BeforeOurTime.Models.Items;
+using BeforeOurTime.Models.Json;
+using BeforeOurTime.Models.Messages.Events;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeforeOurTime.Models.Messages.Locations.ReadLocation
+{
+    /// <summary>
+    /// An location item has been created
+    /// </summary>
+    public class ReadLocationEvent : Event, IEvent
+    {
+        /// <summary>
+        /// Our unique message identifier
+        /// </summary>
+        [JsonIgnore]
+        [JsonConverter(typeof(GuidJsonConverter))]
+        public static Guid _Id = new Guid("81b55604-4b43-46c7-bef7-40a1ba4c8d93");
+        /// <summary>
+        /// Location item
+        /// </summary>
+        [JsonProperty(PropertyName = "location", Order = 1000)]
+        public Item Location { set; get; }
+        /// <summary>
+        /// Exits from location
+        /// </summary>
+        [JsonProperty(PropertyName = "exits", Order = 1100)]
+        public List<Item> Exits { set; get; }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ReadLocationEvent()
+        {
+            this.MessageId = _Id;
+            this.MessageName = "Location Item Has Been Created";
+        }
+    }
+}
---
+++ Messages/Locations/ReadLocation/ReadLocationRequest.cs
@@ -0,0 +1,33 @@
+﻿using BeforeOurTime.Models.Json;
+using BeforeOurTime.Models.Messages.Requests;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeforeOurTime.Models.Messages.Locations.Locations.ReadLocation
+{
+    public class ReadLocationRequest : Request, IRequest
+    {
+        /// <summary>
+        /// Our unique message identifier
+        /// </summary>
+        [JsonIgnore]
+        [JsonConverter(typeof(GuidJsonConverter))]
+        public static Guid _Id = new Guid("272baa24-6725-4e97-9af7-3bc816209e5d");
+        /// <summary>
+        /// List of location items to read
+        /// </summary>
+        [JsonConverter(typeof(GuidJsonConverter))]
+        [JsonProperty(PropertyName = "itemIds", Order = 1000)]
+        public List<Guid> ItemIds { set; get; }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ReadLocationRequest()
+        {
+            this.MessageId = _Id;
+            this.MessageName = "Read Location Request";
+        }
+    }
+}
