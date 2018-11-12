8b31d3dca03c39ef3a060786cabfa1451750c9bf
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2018-11-11 20:59:22 -0800

    Adding new exception

---
+++ Exceptions/BotAuthorizationDeniedException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeforeOurTime.Models.Exceptions
+{
+    /// <summary>
+    /// Base exception for all Before Our Time database related exceptions
+    /// </summary>
+    public class BotAuthorizationDeniedException : BeforeOurTimeException
+    {
+        public BotAuthorizationDeniedException()
+        {
+        }
+
+        public BotAuthorizationDeniedException(string message)
+            : base(message)
+        {
+        }
+
+        public BotAuthorizationDeniedException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
