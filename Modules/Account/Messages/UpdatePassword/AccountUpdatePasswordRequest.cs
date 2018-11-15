9683f67dcf704042b88b3185d5aedead18eafc3f
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2018-11-14 23:27:02 -0800

    Adding message for account password update

---
+++ Modules/Account/Messages/UpdatePassword/AccountUpdatePasswordRequest.cs
@@ -0,0 +1,42 @@
+﻿using BeforeOurTime.Models.Json;
+using BeforeOurTime.Models.Messages.Requests;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeforeOurTime.Models.Modules.Account.Messages.UpdatePassword
+{
+    public class AccountUpdatePasswordRequest : Request, IRequest
+    {
+        /// <summary>
+        /// Our unique message identifier
+        /// </summary>
+        [JsonIgnore]
+        [JsonConverter(typeof(GuidJsonConverter))]
+        public static Guid _Id = new Guid("d0269393-51f4-472b-ad96-40761868addf");
+        /// <summary>
+        /// Account id to update
+        /// </summary>
+        [JsonProperty(PropertyName = "accountId", Order = 1000)]
+        public Guid AccountId { set; get; }
+        /// <summary>
+        /// Old password
+        /// </summary>
+        [JsonProperty(PropertyName = "oldPassword", Order = 1100)]
+        public string OldPassword { set; get; }
+        /// <summary>
+        /// New password
+        /// </summary>
+        [JsonProperty(PropertyName = "newPassword", Order = 1200)]
+        public string NewPassword { set; get; }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public AccountUpdatePasswordRequest()
+        {
+            this.MessageId = _Id;
+            this.MessageName = "Account Update Password Request";
+        }
+    }
+}
---
+++ Modules/Account/Messages/UpdatePassword/AccountUpdatePasswordResponse.cs
@@ -0,0 +1,35 @@
+﻿using BeforeOurTime.Models.Json;
+using BeforeOurTime.Models.Messages.Responses;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeforeOurTime.Models.Modules.Account.Messages.UpdatePassword
+{
+    /// <summary>
+    /// Account update password processed
+    /// </summary>
+    public class AccountUpdatePasswordResponse : Response, IResponse
+    {
+        /// <summary>
+        /// Our unique message identifier
+        /// </summary>
+        [JsonIgnore]
+        [JsonConverter(typeof(GuidJsonConverter))]
+        public static Guid _Id = new Guid("be456a97-4e5f-4ea7-95d8-384819053fb6");
+        /// <summary>
+        /// Account after password update
+        /// </summary>
+        [JsonProperty("account", Order = 1000)]
+        public BeforeOurTime.Models.Modules.Account.Models.Account Account { set; get; }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public AccountUpdatePasswordResponse()
+        {
+            this.MessageId = _Id;
+            this.MessageName = "Account Update Password Response";
+        }
+    }
+}
