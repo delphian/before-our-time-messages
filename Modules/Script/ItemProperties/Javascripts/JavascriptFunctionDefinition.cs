3ae3dae8f7a39e531eec8a7ce920a2ee6995503f
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2019-01-23 23:10:32 -0800

    Each javascript function can now be declared in it's own file

---
+++ Modules/Script/ItemProperties/Javascripts/JavascriptFunctionDefinition.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeforeOurTime.Models.Modules.Script.ItemProperties.Javascripts
+{
+    public class JavascriptFunctionDefinition
+    {
+        public string Prototype { set; get; }
+        public bool Global { set; get; }
+    }
+}
